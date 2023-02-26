c41687ab10ae82f0158501d8612302105f79b31d
Elena Pavliukevich <elenaivanovna0585@gmail.com>
2023-02-26 20:47:25 +0300

    MethodArray

---
+++ Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 65, 67, 3, 25, 94, 47, 21 };
+
+int n = array.Length;
+int find = 94;
+
+int index = 0;
+
+while (index < n)
+{
+  if(array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+  index++;
+}
